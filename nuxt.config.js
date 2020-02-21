72fe015cf33383d6d8d12990832d0bef49e62d2a
David Miller <david.miller@getg5.com>
2020-02-21 12:34:48 -0800

    All the pieces

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,66 +1,64 @@
-
 module.exports = {
   mode: 'spa',
-  /*
-  ** Headers of the page
-  */
+  server: {
+    host: process.env.BASE_URL || 'localhost',
+    port: process.env.PORT || 2020
+  },
   head: {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
+  loading: { color: '#21222e' },
+  loadingIndicator: {
+    name: 'three-bounce',
+    color: '#21222e',
+    background: '#f2f2f2'
+  },
+  css: ['@/assets/main.scss'],
+  plugins: ['@/plugins/apex-chart.js'],
+  buildModules: ['@nuxtjs/eslint-module'],
   modules: [
-    // Doc: https://bootstrap-vue.js.org
     'bootstrap-vue/nuxt',
-    // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+    '@nuxtjs/google-analytics'
   ],
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
-  axios: {
+  googleAnalytics: {
+    id: '',
+    debug: {
+      enabled: false,
+      sendHitTask: true
+    }
   },
-  /*
-  ** Build configuration
-  */
+  bootstrapVue: {
+    bootstrapCSS: false,
+    bootstrapVueCSS: false,
+    config: {}
+  },
+  axios: { browserBaseURL: `//${process.env.BROWSER_URL}` },
   build: {
-    /*
-    ** You can extend webpack config here
-    */
     extend (config, ctx) {
+      if (ctx.isDev) {
+        config.devtool = ctx.isClient
+          ? 'source-map'
+          : 'inline-source-map'
+      }
     }
   }
 }
